593e0d54a08d2f7b24159e5ad0c9a218e84b8304
drmayankvats09 <dr.mayankvats09@gmail.com>
2026-03-26 02:08:14 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -7,13 +7,10 @@ const logger = winston.createLogger({
     winston.format.json()
   ),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
   ],
 });
-
-logger.add(new winston.transports.Console({
-  format: winston.format.simple(),
-}));
 
 module.exports = logger;
