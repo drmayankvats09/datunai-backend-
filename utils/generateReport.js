e07f075f632a24594b53afdd6f1ccc1236a17115
drmayankvats09 <dr.mayankvats09@gmail.com>
2026-04-02 14:22:30 +0530

    Update generateReport.js

--- utils/generateReport.js
+++ utils/generateReport.js
@@ -350,7 +350,7 @@ function generateConsultationPDF(data, stream) {
   checkPage(120);
   
   // Push to bottom area
-  if(doc.y<690)doc.y=690;
+  if(doc.y<725)doc.y=725;
   
   doc.moveTo(M,doc.y).lineTo(W-M,doc.y).lineWidth(0.3).strokeColor(grayBdr).stroke();
   doc.moveDown(0.4);
